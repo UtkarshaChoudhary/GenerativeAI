45d7d11a27fd7c4e33fe8ae831f23ad2552020d9
Utkarsha Sukhsohale <98605352+UtkarshaChoudhary@users.noreply.github.com>
2025-02-13 11:02:02 +0000

    Add files via upload

---
+++ Simple Q&A App with Groq API/streamlit_code.py
@@ -0,0 +1,21 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+import os
+from langchain_community.chat_models import ChatOllama
+import streamlit as st
+from langchain.globals import set_debug
+
+set_debug(True)
+
+GROQ_API_KEY= os.getenv("GROQ_API_KEY")
+
+llm=ChatGroq(model="llama-3.3-70b-versatile",api_key= GROQ_API_KEY)
+
+st.title("Ask Anything :)")
+
+question = st.text_input("Enter the question:")
+
+if question:
+    response=llm.invoke(question)
+    st.write(response.content)
